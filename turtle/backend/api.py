fb6200c60daa87aea85a2cc86e11c6c4700fe9d2
Antoine.Yebouet <antoine.yebouet@telecomnancy.eu>
2023-03-30 11:55:35 +0200

    api routes

--- turtle/backend/api.py
+++ turtle/backend/api.py
@@ -1,3 +1,35 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from apidb import *
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(getAllTasks())
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.get_json()
+    author = data['author']
+    title = data['title']
+    description = data['description']
+    price = data['price']
+    addTask(author, title, description, price)
+    return jsonify({'message': 'Task added'}),200
+
+@app.route('/accept', methods=['POST'])
+def accept_task():
+    data = request.get_json()
+    acceptor = data['acceptor']
+    task_id = data['task_id']
+    acceptTask(acceptor, task_id)
+    return jsonify({'message': 'Task accepted'}),200
+
+@app.route('/complete', methods=['POST'])
+def complete_task():
+    data = request.get_json()
+    task_id = data['task_id']
+    completeTask(task_id)
+    return jsonify({'message': 'Task completed'}),200
